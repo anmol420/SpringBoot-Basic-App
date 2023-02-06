fece7fa318230bf1f5b7ff8a05a8cb45d00ec9a0
Anmol Anand <anmolanand2k15@gmail.com>
2023-02-06 16:59:30 +0530

    Added Api Layer

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Anmol Anand",
+                        18,
+                        LocalDate.of(2004,12,4),
+                        "anmolanand2k15@gmail.com"
+                )
+        );
+    }
+}
